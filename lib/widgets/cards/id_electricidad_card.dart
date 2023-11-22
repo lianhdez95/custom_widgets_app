54e3e30ce60f92ba74c09363160adec13cd6f31e
lian <lfhernandez@xetid.cu>
2023-11-22 00:47:18 -0500

    changes to electricity widget

--- lib/widgets/cards/id_electricidad_card.dart
+++ lib/widgets/cards/id_electricidad_card.dart
@@ -18,26 +18,43 @@ class IdElectricidadCard extends StatelessWidget {
                 // side: const BorderSide(color: Colors.orange),
                 borderRadius: BorderRadius.circular(10)),
             child: Container(
-              padding: EdgeInsets.all(width*0.03),
+              padding: EdgeInsets.all(width * 0.03),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Image.asset('assets/images/ic_pago_electricidad.png', height: height*0.08, width: height*0.08,),
+                  Opacity(
+                    opacity: 0.5,
+                    child: Image.asset(
+                      'assets/images/ic_pago_electricidad.png',
+                      height: height * 0.08,
+                      width: height * 0.08,
+                    ),
+                  ),
+                  SizedBox(
+                    width: width * 0.05,
+                  ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text('Nombre de cliente',
-                          style: TextStyle(fontSize: width * 0.05)),
+                          style: TextStyle(fontSize: width * 0.04)),
                       Text("Id del cliente",
-                          style: TextStyle(fontSize: width * 0.04, color: Colors.grey[600])),
+                          style: TextStyle(
+                              fontSize: width * 0.04, color: Colors.grey[600])),
                     ],
                   ),
-                  IconButton(
-                    onPressed: () {},
-                    splashRadius: height*0.03,
-                    splashColor: Colors.orange[200],
-                    icon: const Icon(Icons.delete),
-                    color: Colors.orange[600],
+                  SizedBox(
+                    width: width * 0.15,
+                  ),
+                  Opacity(
+                    opacity: 0.5,
+                    child: IconButton(
+                      onPressed: () {},
+                      splashRadius: height * 0.03,
+                      splashColor: Colors.orange[200],
+                      icon: Icon(Icons.delete, size: height * 0.04),
+                      color: Colors.orange[600],
+                    ),
                   ),
                 ],
               ),
